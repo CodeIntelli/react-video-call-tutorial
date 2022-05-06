c5c5a1f18a145cafe5702923b85395ccf25ad698
DUSHYANT KHODA <dushyant.k@upsquare.in>
2022-05-06 17:44:40 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -89,7 +89,7 @@ if (env === "production") {
   });
 }
 
-const serverPort = process.env.SERVER_PORT || process.env.PORT || 5000;
+const serverPort = process.env.PORT || 5000;
 // start express server on port 5000
 app.listen(serverPort, () => {
   console.log("server started on port", serverPort);
